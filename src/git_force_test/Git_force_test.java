cb786f60cf35ba66e0d324abe6211bd6607f3285
KSZBence <kunszekelybence@gmail.com>
2023-09-25 14:54:26 +0200

    testmerge_jav

--- src/git_force_test/Git_force_test.java
+++ src/git_force_test/Git_force_test.java
@@ -6,13 +6,10 @@ public class Git_force_test {
 
     
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("bbb");
-=======
-            for (int i = 0; i < 10; i++) {
-            
-        }
->>>>>>> 1b73bc2a92969e629488d679f6f7d0d02b96d402
+
+
             
         }
     }
