9c2e08c81c679d09fbf0fab78ed85354f4144eab
KSZBence <kunszekelybence@gmail.com>
2023-09-25 14:39:12 +0200

    for

--- src/git_force_test/Git_force_test.java
+++ src/git_force_test/Git_force_test.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package git_force_test;
 
-/**
- *
- * @author kunszekely.bence
- */
+
 public class Git_force_test {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (Map.Entry<Object, Object> en : m.entrySet()) {
+            Object key = en.getKey();
+            Object value = en.getValue();
+            
+        }
     }
     
 }
